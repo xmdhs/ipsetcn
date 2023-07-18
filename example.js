d336cb47fda4d18e9a49f8d121f7a248c08e7471
xmdhs <xmdhss@gmail.com>
2023-07-19 00:44:35 +0800

    更复杂一点的例子

--- example.js
+++ example.js
@@ -1,5 +1,17 @@
 function need(a, ipPre, is4) {
-    if (!a?.country?.iso_code == "CN") {
+    let n = false
+    if (a?.country?.iso_code == "CN") {
+        n = true
+    }
+    if (a?.country?.iso_code == "JP" && is4) {
+        const l = ipPre.split(".")
+        const l2 = Number(l[1])
+        if (l[0] == "8" && l2 >= 208 && l2 <= 222) {
+            n = true
+        }
+    }
+
+    if (!n) {
         return {
             "tag": "",
             "need": false
